3a71539debb056708a434f274b472ca3c47cedea
Daria Schumm <zonamoroza@gmail.com>
2024-08-07 08:23:19 +1000

    changed text in services

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function Banner () {
   return (
